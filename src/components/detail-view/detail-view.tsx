d7ce10b9beff08cd92122aaa383f4b00da6e0b21
Jasmin Carter <70289696+sleepykanguru22@users.noreply.github.com>
2023-10-25 17:24:31 -0700

    more tests

--- src/components/detail-view/detail-view.tsx
+++ src/components/detail-view/detail-view.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames';
+import React from 'react';
 import styles from './detail-view.module.scss';
+import { data } from '../../data';
 
 export interface DetailViewProps {
     className?: string;
@@ -10,5 +12,9 @@ export interface DetailViewProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const DetailView = ({ className }: DetailViewProps) => {
-    return <div className={classNames(styles.root, className)}>DetailView</div>;
+    return (
+        <div className={classNames(styles.root, className)}>
+            <p></p>
+        </div>
+    );
 };
